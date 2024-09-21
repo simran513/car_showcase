a9b6e198241853ab97d83790a5521321523adcba
simran513 <khandualsimran@gmail.com>
2024-09-21 17:34:09 +0530

    Initial commit

---
+++ components/index.ts
@@ -0,0 +1,11 @@
+import CustomButton from "./CustomButton";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from './SearchManufacturer'
+
+export{
+    Hero, CustomButton, Navbar, Footer, SearchBar, CustomFilter, SearchManufacturer
+}
